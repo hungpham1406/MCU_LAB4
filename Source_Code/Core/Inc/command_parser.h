948d7ccc81b36c3c381c23c788c1d0f3c41a347f
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2023-11-25 13:41:04 +0700

    Init for lab 4

---
+++ Source_Code/Core/Inc/command_parser.h
@@ -0,0 +1,20 @@
+/*
+ * command_parser.h
+ *
+ *  Created on: Nov 25, 2023
+ *      Author: Asus
+ */
+
+#ifndef INC_COMMAND_PARSER_H_
+#define INC_COMMAND_PARSER_H_
+
+# define MAX_BUFFER_SIZE 30
+
+
+
+extern uint8_t temp;
+extern uint8_t buffer [ MAX_BUFFER_SIZE ];
+extern uint8_t index_buffer;
+extern uint8_t buffer_flag;
+
+#endif /* INC_COMMAND_PARSER_H_ */
---
+++ Source_Code/Core/Src/command_parser.c
@@ -0,0 +1,12 @@
+/*
+ * command_parser.c
+ *
+ *  Created on: Nov 25, 2023
+ *      Author: Asus
+ */
+
+uint8_t temp=0;
+uint8_t buffer [ MAX_BUFFER_SIZE ]={0};
+uint8_t index_buffer=0;
+uint8_t buffer_flag=0;
+
